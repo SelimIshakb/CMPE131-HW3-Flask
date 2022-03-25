0d4874fc55b0069f62c29a25c0692bb6d856df14
Selim Ishakbeyoglu <selim.ishakbeyoglu@sjsu.edu>
2022-03-25 02:30:31 -0700

    Added web form for new cities

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from config import Config
 
 name = "Selim"
 city_names = ["Istanbul", "Ankara", "Izmir", "Bursa"]
 myobj = Flask(__name__)
+myobj.config.from_mapping(SECRET_KEY="super secret key")
+
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
 from flask import render_template
 from app import myobj, name, city_names
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 @myobj.route('/')
 def home():
-    return render_template("home.html", name=name, cities=city_names)
+    form = CityForm()
+    return render_template("home.html", name=name, cities=city_names, form=form)
+
+
+class CityForm(FlaskForm):
+    new_city = StringField("City Name", validators=[DataRequired()])
+    submit = SubmitField()
